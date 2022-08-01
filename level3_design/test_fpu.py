4699b103f869a8327c4779f2d2fdff78cc9424cc
Mustafa-abdelhamid <moostafamohamedd@gmail.com>
2022-08-01 18:14:28 +0000

    edits

--- level3_design/test_fpu.py
+++ level3_design/test_fpu.py
@@ -1,60 +1,95 @@
-# See LICENSE.iitm for details
-# See LICENSE.vyoma for details
-import numpy as np
+
+
+import os
 import random
-import sys
+from pathlib import Path
+
 import cocotb
-from cocotb.decorators import coroutine
-from cocotb.triggers import Timer, RisingEdge
-from cocotb.result import TestFailure
 from cocotb.clock import Clock
+from cocotb.triggers import RisingEdge, FallingEdge
 
-#input		clk;
-#input	[1:0]	rmode;
-#input	[2:0]	fpu_op;
-#input	[31:0]	opa, opb;
-
-#output	[31:0]	out;
-#output		inf, snan, qnan;
-#output		ine;
-#output		overflow, underflow;
-#output		zero;
-#output		div_by_zero;
-
-# Clock Generation
-@cocotb.coroutine
-def clock_gen(signal):
-    while True:
-        signal.value <= 0
-        yield Timer(1) 
-        signal.value <= 1
-        yield Timer(1) 
-
-# Sample Test
 @cocotb.test()
-def run_test(dut):
+async def test_seq_bug1(dut):
+    """Test for FPU"""
 
-    # clock
-    cocotb.fork(clock_gen(dut.clk))
+    clock = Clock(dut.clk, 10, units="us")  # Create a 10us period clock on port clk
+    cocotb.start_soon(clock.start())        # Start the clock
 
     
-    ######### CTB : Modify the test to expose the bug #############
-    # input transaction
-    # driving the input transaction
+    
+    await FallingEdge(dut.clk)  
     dut.opcode.value = 0 # ADD
-
     x=random.randint(0,(2**32)-1)
+    x_length=len(bin(x))-2
+    if(x_length < 32):
+        m=32-x_length
+        x_sign=0
+        x_exponent=int(bin(x)[3:11],2)
+        x_exponent=(x_exponent>>m)-127
+        print(x_exponent)
+        x_mantissa=int(bin(x)[12::],2) >>m
+        x_mantissa=x_mantissa* (2**-23)
+        print(x_mantissa)
+
+        if (x_exponent==-127): # denormal
+            if(x_sign==0):
+                x_value= (x_mantissa)*(2**x_exponent)
+            else:
+                x_value= -1*(x_mantissa)*(2**x_exponent)
+
+        else:
+            if(x_sign==0):
+                x_value= (1+x_mantissa)*(2**x_exponent)
+            else:
+                x_value= -1*(1+x_mantissa)*(2**x_exponent)
+    
+    else:
+        x_sign=int(bin(x)[2],2)
+        x_exponent=int(bin(x)[3:11],2)-127
+        print(x_exponent)
+        x_mantissa=int(bin(x)[12::],2) * (2**-23)
+        print(x_mantissa)
+
+        if (x_exponent==-127): # denormal
+            if(x_sign==0):
+                x_value= (x_mantissa)*(2**x_exponent)
+            else:
+                x_value= -1*(x_mantissa)*(2**x_exponent)
+
+        else:
+            if(x_sign==0):
+                x_value= (1+x_mantissa)*(2**x_exponent)
+            else:
+                x_value= -1*(1+x_mantissa)*(2**x_exponent)
+
+    print(x_value)
+    dut.A.value=x
     y=random.randint(0,(2**32)-1)
-    
-    
-    dut.A.value =x
     print(bin(x))
     dut.B.value =y
     print(bin(y))
-
-    # obtaining the output
-    yield Timer(12) 
-
+    await FallingEdge(dut.clk)
     dut_output = dut.O.value
     print(dut_output)
+    print(bin(x))
+    print(bin(y))
+
+    cocotb.log.info('#### CTB: Develop your test here! ######')
+    
+
+    
+    
+    
+     
+     
+# See LICENSE.iitm for details
+# See LICENSE.vyoma for details
+#import numpy as np
+#import random
+#import sys
+#import cocotb
+#from cocotb.decorators import coroutine
+#from cocotb.triggers import Timer, RisingEdge
+#from cocotb.result import TestFailure
+#from cocotb.clock import Clock
    
